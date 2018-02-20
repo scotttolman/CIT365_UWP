b41770bd548bd2b2bb8a979f8707147ce5326530
scotttolman <scotttolman@gmail.com>
2018-02-19 23:45:05 -0600

    Add project files.

---
+++ GlazerCalculation/Glass.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlazerCalculation
+{
+    public enum TintType { black, brown, blue };
+    public class Glass
+    {
+        public const double MIN_WIDTH = 0.5;
+        public const double MAX_WIDTH = 5.0;
+        public const double MIN_HEIGHT = 0.75;
+        public const double MAX_HEIGHT = 3.0;
+
+
+        double width;
+        double height;
+        TintType tint;
+        double woodLength;
+        double glassArea;
+        DateTime dateOrdered;
+        double quantity;
+
+        public Glass(double width, double height, TintType tint, double quantity)
+        {
+            this.width = width;
+            this.height = height;
+            this.tint = tint;
+            this.woodLength = 2 * (width + height) * 3.25;
+            this.glassArea = 2 * (width * height);
+            dateOrdered = DateTime.Now;
+            this.quantity = quantity;
+        }
+
+        public string display()
+        {
+            return "Width = " + width + "\nHeight = " + height + "\nTint = " + Enum.GetName(typeof(TintType), tint) + 
+                "\nQuantity = " + quantity + "\nWood Length = " + woodLength + "\nGlass Area = " + glassArea + 
+                "\nDate = " + dateOrdered;
+        }
+
+        public void setWidth(double width)
+        {
+            if (width > MIN_WIDTH && width < MAX_WIDTH)
+                this.width = width;
+        }
+
+        public double getWidth()
+        {
+            return width;
+        }
+
+        public void setHeight(double height)
+        {
+            if (height > MIN_HEIGHT && height < MAX_HEIGHT)
+                this.height = height;
+        }
+
+        public double getHEIGHT()
+        {
+            return height;
+        }
+
+        public void setTint(TintType tint)
+        {
+            this.tint = tint;
+        }
+
+        public TintType getTint()
+        {
+            return tint;
+        }
+
+        public void setQuantity(int quantity)
+        {
+            if (quantity > 0 && quantity < 11)
+            {
+                this.quantity = quantity;
+            }
+        }
+
+        public double getQuantity()
+        {
+            return quantity;
+        }
+    }
+}
---
+++ GlazerCalculation/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace GlazerCalculation
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            List<TintType> list = Enum.GetValues(typeof(TintType)).Cast<TintType>().ToList();
+            tintBox.ItemsSource = list;
+        }
+
+        private void widthBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            float input;
+            if (float.TryParse(widthBox.Text, out input))
+            {
+                if (input < Glass.MIN_WIDTH || input > Glass.MAX_WIDTH)
+                {
+                    valWidth.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    valWidth.Visibility = Visibility.Collapsed;
+                }
+            }
+            else
+            {
+                valWidth.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void heightBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            float input;
+            if (float.TryParse(heightBox.Text, out input))
+            {
+                if (input < Glass.MIN_HEIGHT || input > Glass.MAX_HEIGHT)
+                {
+                    valHeight.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    valHeight.Visibility = Visibility.Collapsed;
+                }
+            }
+            else
+            {
+                valHeight.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Glass glass = new Glass(double.Parse(widthBox.Text), double.Parse(heightBox.Text), 
+                (TintType)tintBox.SelectedValue, quantSlider.Value);
+            string calculation = glass.display();
+
+            showResults(calculation);
+        }
+
+        private async void showResults(string calculation)
+        {
+            ContentDialog results = new ContentDialog
+            {
+                Title = "Results",
+                Content = calculation,
+                CloseButtonText = "Ok"
+            };
+
+            ContentDialogResult result = await results.ShowAsync();
+        }
+    }
+}
